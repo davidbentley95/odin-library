25a4df4eb08b472ce640e6f9df930e8b87f82372
DBENTLEY <david.bentley95@gmail.com>
2025-01-10 06:57:54 -0800

    Prototype create book functionality
    Prevent default form behavior to avoid page refreshes and
    submit attempts.
    Add event listener to create book button (in modal) which
    closes will extract form details, close the modal, and clear
    form details.

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const LIBRARY = [];
 
+const addButton = document.querySelector(".add-button");
+const modal = document.querySelector("dialog");
+const createButton = document.querySelector(".create-book");
+const form = document.querySelector(".new-book-form");
+
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
 }
+
+console.log(form);
+
+function getFormInputs(form) {
+
+}
+
+// EVENT LISTENERS
+addButton.addEventListener("click", () => modal.showModal());
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+createButton.addEventListener("click", () => {
+    // add new book to library
+    modal.close()
+    // clear form content
+});
