b8f566863783036145b7bec84764d0184b33a203
DBENTLEY <david.bentley95@gmail.com>
2025-01-11 06:14:21 -0800

    Implement createBookDiv function
    Add functionality that creates a book div and displays on page
    after pushing the add book button on modal popup.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const addButton = document.querySelector(".add-button");
 const modal = document.querySelector("dialog");
 const createButton = document.querySelector(".create-book");
 const form = document.querySelector(".new-book-form");
+const bookCarousel = document.querySelector(".book-carousel");
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -20,9 +21,27 @@ function addBookToLibrary() {
         true
     )
     LIBRARY.push(book);
+    createBookDiv(book);
 }
 
-function createBookDiv() {
+function createBookDiv(bookObject) {
+    const newBook = document.createElement("div");
+    const title = document.createElement("h3")
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    
+    newBook.classList.add("book-card");
+    title.classList.add("book-title");
+    author.classList.add("book-author");
+    pages.classList.add("book-pages");
+
+    title.innerHTML = bookObject.title;
+    author.innerHTML = bookObject.author;
+    pages.innerHTML = bookObject.pages;
+
+    newBook.append(title, author, pages);
+
+    bookCarousel.appendChild(newBook);
 
 }
 
@@ -43,3 +62,5 @@ createButton.addEventListener("click", () => {
     // clear form content
     clearForm();
 });
+
+
