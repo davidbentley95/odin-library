5fa1c1f00c606c14c11a9484fa1970c37b2db504
DBENTLEY <david.bentley95@gmail.com>
2025-01-12 06:30:07 -0800

    Implement read status checkbox
    Add functionality to specify if a book has been read or not.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function addBookToLibrary() {
         form.querySelector("#title").value,
         form.querySelector("#author").value,
         form.querySelector("#pages").value,
-        true
+        form.querySelector("#read-status-modal").checked
     )
     LIBRARY.push(book);
     createBookDiv(book);
@@ -29,20 +29,33 @@ function createBookDiv(bookObject) {
     const title = document.createElement("h3")
     const author = document.createElement("p");
     const pages = document.createElement("p");
+    const deleteButton = document.createElement("button");
+    const readStatus = document.createElement("div");
+    const readLabel = document.createElement("label");
+    const readCheckBox = document.createElement("input");
     
     newBook.classList.add("book-card");
     title.classList.add("book-title");
     author.classList.add("book-author");
     pages.classList.add("book-pages");
+    deleteButton.classList.add("delete-button");
+    readStatus.id = "read-status";
+
+    readCheckBox.type = "checkbox";
 
     title.innerHTML = bookObject.title;
     author.innerHTML = bookObject.author;
     pages.innerHTML = bookObject.pages;
+    deleteButton.innerHTML = "DELETE BOOK";
+    readCheckBox.checked = bookObject.readStatus;
 
-    newBook.append(title, author, pages);
+    readLabel.htmlFor = "read-status";
 
-    bookCarousel.appendChild(newBook);
+    readStatus.append(readLabel, readCheckBox);
 
+    newBook.append(title, author, pages, readStatus, deleteButton);
+
+    bookCarousel.append(newBook);
 }
 
 function clearForm() {
@@ -56,11 +69,17 @@ addButton.addEventListener("click", () => modal.showModal());
 createButton.addEventListener("click", () => {
     // add new book to library
     addBookToLibrary();
-    console.table(LIBRARY);
     // close modal
     modal.close();
     // clear form content
     clearForm();
 });
 
+document.querySelector(".book-carousel").addEventListener('click', function(event) {
+    if (event.target.classList.contains('delete-button')) {
+        // Handle delete
+        console.log(event.target.parentNode);
+        event.target.parentNode.remove(); // Example: Remove the parent element
+    }
+});
 
