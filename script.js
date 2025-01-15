1ca82fa92af1e7494b29d3b227af1ab10ce94d60
DBENTLEY <david.bentley95@gmail.com>
2025-01-15 07:16:33 -0800

    Add styling to page
    Start adding styling to page following a neumorphism design.

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function createBookDiv(bookObject) {
     svgElement.setAttribute("viewBox", "0 0 20 19");
     svgElement.setAttribute("fill", "none");
     pathElement.setAttribute("d", "M8.88659 16.6603L8.88587 16.6596C6.30104 14.3157 4.19578 12.4033 2.73088 10.6111C1.27148 8.82559 0.5 7.22062 0.5 5.5C0.5 2.68674 2.69555 0.5 5.5 0.5C7.08885 0.5 8.62206 1.24223 9.62058 2.40564L10 2.84771L10.3794 2.40564C11.3779 1.24223 12.9112 0.5 14.5 0.5C17.3045 0.5 19.5 2.68674 19.5 5.5C19.5 7.22062 18.7285 8.82559 17.2691 10.6111C15.8042 12.4033 13.699 14.3157 11.1141 16.6596L11.1134 16.6603L10 17.6738L8.88659 16.6603Z");
-    pathElement.setAttribute("stroke", "black");
+    pathElement.setAttribute("stroke", "white");
     favouriteButton.setAttribute("name", "favourite");
 
     readCheckbox.type = "checkbox";
@@ -171,9 +171,9 @@ function changeHeartColor() {
         const favouriteIcon = item.querySelector("path");
 
         if(favouriteButton.checked) {
-           favouriteIcon.setAttribute("fill", "black");
+           favouriteIcon.setAttribute("fill", "white");
         } else {
-            favouriteIcon.setAttribute("fill", "white");
+            favouriteIcon.setAttribute("fill", "black");
         }
     });
 }
